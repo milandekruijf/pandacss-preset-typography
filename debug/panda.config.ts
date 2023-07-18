a5fb42d306773d06146b07466e48e4702c6eb17d
Muijf <admin@muijf.com>
2023-07-18 16:29:42 +0200

    added debug folder

---
+++ debug/panda.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@pandacss/dev";
+
+// import typographyPreset from "../dist";
+
+export default defineConfig({
+  presets: ["@pandacss/preset-panda"],
+  outdir: "debug/out",
+});
