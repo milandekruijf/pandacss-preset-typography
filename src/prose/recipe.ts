2b8a65c3af27b897740fb3b1af287522c5e98227
Muijf <admin@muijf.com>
2023-07-18 22:55:40 +0200

    added all styles to prose

--- src/prose/recipe.ts
+++ src/prose/recipe.ts
@@ -23,6 +23,10 @@ export function createProseRecipe(options?: ProseRecipeOptions): RecipeConfig {
 
   return defineRecipe({
     name,
+    jsx,
+    defaultVariants: {
+      size: "base",
+    },
     base: {
       color: "prose.body",
       maxWidth: "65ch",
@@ -574,13 +578,524 @@ export function createProseRecipe(options?: ProseRecipeOptions): RecipeConfig {
             marginBottom: "0",
           },
         },
-        lg: {},
+        lg: {
+          fontSize: rem(18),
+          lineHeight: round(32 / 18),
+          "& p": {
+            marginTop: em(24, 18),
+            marginBottom: em(24, 18),
+          },
+          '& [class~="lead"]': {
+            fontSize: em(22, 18),
+            lineHeight: round(32 / 22),
+            marginTop: em(24, 22),
+            marginBottom: em(24, 22),
+          },
+          "& blockquote": {
+            marginTop: em(40, 24),
+            marginBottom: em(40, 24),
+            paddingLeft: em(24, 24),
+          },
+          "& h1": {
+            fontSize: em(48, 18),
+            marginTop: "0",
+            marginBottom: em(40, 48),
+            lineHeight: round(48 / 48),
+          },
+          "& h2": {
+            fontSize: em(30, 18),
+            marginTop: em(56, 30),
+            marginBottom: em(32, 30),
+            lineHeight: round(40 / 30),
+          },
+          "& h3": {
+            fontSize: em(24, 18),
+            marginTop: em(40, 24),
+            marginBottom: em(16, 24),
+            lineHeight: round(36 / 24),
+          },
+          "& h4": {
+            marginTop: em(32, 18),
+            marginBottom: em(8, 18),
+            lineHeight: round(28 / 18),
+          },
+          "& img": {
+            marginTop: em(32, 18),
+            marginBottom: em(32, 18),
+          },
+          "& video": {
+            marginTop: em(32, 18),
+            marginBottom: em(32, 18),
+          },
+          "& figure": {
+            marginTop: em(32, 18),
+            marginBottom: em(32, 18),
+          },
+          "& figure > *": {
+            marginTop: "0",
+            marginBottom: "0",
+          },
+          "& figcaption": {
+            fontSize: em(16, 18),
+            lineHeight: round(24 / 16),
+            marginTop: em(16, 16),
+          },
+          "& code": {
+            fontSize: em(16, 18),
+          },
+          "& h2 code": {
+            fontSize: em(26, 30),
+          },
+          "& h3 code": {
+            fontSize: em(21, 24),
+          },
+          "& pre": {
+            fontSize: em(16, 18),
+            lineHeight: round(28 / 16),
+            marginTop: em(32, 16),
+            marginBottom: em(32, 16),
+            borderRadius: rem(6),
+            paddingTop: em(16, 16),
+            paddingRight: em(24, 16),
+            paddingBottom: em(16, 16),
+            paddingLeft: em(24, 16),
+          },
+          "& ol": {
+            marginTop: em(24, 18),
+            marginBottom: em(24, 18),
+            paddingLeft: em(28, 18),
+          },
+          "& ul": {
+            marginTop: em(24, 18),
+            marginBottom: em(24, 18),
+            paddingLeft: em(28, 18),
+          },
+          "& li": {
+            marginTop: em(12, 18),
+            marginBottom: em(12, 18),
+          },
+          "& ol > li": {
+            paddingLeft: em(8, 18),
+          },
+          "& ul > li": {
+            paddingLeft: em(8, 18),
+          },
+          "& > ul > li p": {
+            marginTop: em(16, 18),
+            marginBottom: em(16, 18),
+          },
+          "& > ul > li > *:first-child": {
+            marginTop: em(24, 18),
+          },
+          "& > ul > li > *:last-child": {
+            marginBottom: em(24, 18),
+          },
+          "& > ol > li > *:first-child": {
+            marginTop: em(24, 18),
+          },
+          "& > ol > li > *:last-child": {
+            marginBottom: em(24, 18),
+          },
+          "& ul ul, & ul ol, & ol ul, & ol ol": {
+            marginTop: em(16, 18),
+            marginBottom: em(16, 18),
+          },
+          "& hr": {
+            marginTop: em(56, 18),
+            marginBottom: em(56, 18),
+          },
+          "& hr + *": {
+            marginTop: "0",
+          },
+          "& h2 + *": {
+            marginTop: "0",
+          },
+          "& h3 + *": {
+            marginTop: "0",
+          },
+          "& h4 + *": {
+            marginTop: "0",
+          },
+          "& table": {
+            fontSize: em(16, 18),
+            lineHeight: round(24 / 16),
+          },
+          "& thead th": {
+            paddingRight: em(12, 16),
+            paddingBottom: em(12, 16),
+            paddingLeft: em(12, 16),
+          },
+          "& thead th:first-child": {
+            paddingLeft: "0",
+          },
+          "& thead th:last-child": {
+            paddingRight: "0",
+          },
+          "& tbody td, & tfoot td": {
+            paddingTop: em(12, 16),
+            paddingRight: em(12, 16),
+            paddingBottom: em(12, 16),
+            paddingLeft: em(12, 16),
+          },
+          "& tbody td:first-child, & tfoot td:first-child": {
+            paddingLeft: "0",
+          },
+          "& tbody td:last-child, & tfoot td:last-child": {
+            paddingRight: "0",
+          },
+          "& > :first-child": {
+            marginTop: "0",
+          },
+          "& > :last-child": {
+            marginBottom: "0",
+          },
+        },
+        xl: {
+          fontSize: rem(20),
+          lineHeight: round(36 / 20),
+          "& p": {
+            marginTop: em(24, 20),
+            marginBottom: em(24, 20),
+          },
+          '& [class~="lead"]': {
+            fontSize: em(24, 20),
+            lineHeight: round(36 / 24),
+            marginTop: em(24, 24),
+            marginBottom: em(24, 24),
+          },
+          "& blockquote": {
+            marginTop: em(48, 30),
+            marginBottom: em(48, 30),
+            paddingLeft: em(32, 30),
+          },
+          "& h1": {
+            fontSize: em(56, 20),
+            marginTop: "0",
+            marginBottom: em(48, 56),
+            lineHeight: round(56 / 56),
+          },
+          "& h2": {
+            fontSize: em(36, 20),
+            marginTop: em(56, 36),
+            marginBottom: em(32, 36),
+            lineHeight: round(40 / 36),
+          },
+          "& h3": {
+            fontSize: em(30, 20),
+            marginTop: em(48, 30),
+            marginBottom: em(20, 30),
+            lineHeight: round(40 / 30),
+          },
+          "& h4": {
+            marginTop: em(36, 20),
+            marginBottom: em(12, 20),
+            lineHeight: round(32 / 20),
+          },
+          "& img": {
+            marginTop: em(40, 20),
+            marginBottom: em(40, 20),
+          },
+          "& video": {
+            marginTop: em(40, 20),
+            marginBottom: em(40, 20),
+          },
+          "& figure": {
+            marginTop: em(40, 20),
+            marginBottom: em(40, 20),
+          },
+          "& figure > *": {
+            marginTop: "0",
+            marginBottom: "0",
+          },
+          "& figcaption": {
+            fontSize: em(18, 20),
+            lineHeight: round(28 / 18),
+            marginTop: em(18, 18),
+          },
+          "& code": {
+            fontSize: em(18, 20),
+          },
+          "& h2 code": {
+            fontSize: em(31, 36),
+          },
+          "& h3 code": {
+            fontSize: em(27, 30),
+          },
+          "& pre": {
+            fontSize: em(18, 20),
+            lineHeight: round(32 / 18),
+            marginTop: em(36, 18),
+            marginBottom: em(36, 18),
+            borderRadius: rem(8),
+            paddingTop: em(20, 18),
+            paddingRight: em(24, 18),
+            paddingBottom: em(20, 18),
+            paddingLeft: em(24, 18),
+          },
+          "& ol": {
+            marginTop: em(24, 20),
+            marginBottom: em(24, 20),
+            paddingLeft: em(32, 20),
+          },
+          "& ul": {
+            marginTop: em(24, 20),
+            marginBottom: em(24, 20),
+            paddingLeft: em(32, 20),
+          },
+          "& li": {
+            marginTop: em(12, 20),
+            marginBottom: em(12, 20),
+          },
+          "& ol > li": {
+            paddingLeft: em(8, 20),
+          },
+          "& ul > li": {
+            paddingLeft: em(8, 20),
+          },
+          "& > ul > li p": {
+            marginTop: em(16, 20),
+            marginBottom: em(16, 20),
+          },
+          "& > ul > li > *:first-child": {
+            marginTop: em(24, 20),
+          },
+          "& > ul > li > *:last-child": {
+            marginBottom: em(24, 20),
+          },
+          "& > ol > li > *:first-child": {
+            marginTop: em(24, 20),
+          },
+          "& > ol > li > *:last-child": {
+            marginBottom: em(24, 20),
+          },
+          "& ul ul, & ul ol, & ol ul, & ol ol": {
+            marginTop: em(16, 20),
+            marginBottom: em(16, 20),
+          },
+          "& hr": {
+            marginTop: em(56, 20),
+            marginBottom: em(56, 20),
+          },
+          "& hr + *": {
+            marginTop: "0",
+          },
+          "& h2 + *": {
+            marginTop: "0",
+          },
+          "& h3 + *": {
+            marginTop: "0",
+          },
+          "& h4 + *": {
+            marginTop: "0",
+          },
+          "& table": {
+            fontSize: em(18, 20),
+            lineHeight: round(28 / 18),
+          },
+          "& thead th": {
+            paddingRight: em(12, 18),
+            paddingBottom: em(16, 18),
+            paddingLeft: em(12, 18),
+          },
+          "& thead th:first-child": {
+            paddingLeft: "0",
+          },
+          "& thead th:last-child": {
+            paddingRight: "0",
+          },
+          "& tbody td, & tfoot td": {
+            paddingTop: em(16, 18),
+            paddingRight: em(12, 18),
+            paddingBottom: em(16, 18),
+            paddingLeft: em(12, 18),
+          },
+          "& tbody td:first-child, & tfoot td:first-child": {
+            paddingLeft: "0",
+          },
+          "& tbody td:last-child, & tfoot td:last-child": {
+            paddingRight: "0",
+          },
+          "& > :first-child": {
+            marginTop: "0",
+          },
+          "& > :last-child": {
+            marginBottom: "0",
+          },
+        },
+        "2xl": {
+          fontSize: rem(24),
+          lineHeight: round(40 / 24),
+          "& p": {
+            marginTop: em(32, 24),
+            marginBottom: em(32, 24),
+          },
+          '& [class~="lead"]': {
+            fontSize: em(30, 24),
+            lineHeight: round(44 / 30),
+            marginTop: em(32, 30),
+            marginBottom: em(32, 30),
+          },
+          "& blockquote": {
+            marginTop: em(64, 36),
+            marginBottom: em(64, 36),
+            paddingLeft: em(40, 36),
+          },
+          "& h1": {
+            fontSize: em(64, 24),
+            marginTop: "0",
+            marginBottom: em(56, 64),
+            lineHeight: round(64 / 64),
+          },
+          "& h2": {
+            fontSize: em(48, 24),
+            marginTop: em(72, 48),
+            marginBottom: em(40, 48),
+            lineHeight: round(52 / 48),
+          },
+          "& h3": {
+            fontSize: em(36, 24),
+            marginTop: em(56, 36),
+            marginBottom: em(24, 36),
+            lineHeight: round(44 / 36),
+          },
+          "& h4": {
+            marginTop: em(40, 24),
+            marginBottom: em(16, 24),
+            lineHeight: round(36 / 24),
+          },
+          "& img": {
+            marginTop: em(48, 24),
+            marginBottom: em(48, 24),
+          },
+          "& video": {
+            marginTop: em(48, 24),
+            marginBottom: em(48, 24),
+          },
+          "& figure": {
+            marginTop: em(48, 24),
+            marginBottom: em(48, 24),
+          },
+          "& figure > *": {
+            marginTop: "0",
+            marginBottom: "0",
+          },
+          "& figcaption": {
+            fontSize: em(20, 24),
+            lineHeight: round(32 / 20),
+            marginTop: em(20, 20),
+          },
+          "& code": {
+            fontSize: em(20, 24),
+          },
+          "& h2 code": {
+            fontSize: em(42, 48),
+          },
+          "& h3 code": {
+            fontSize: em(32, 36),
+          },
+          "& pre": {
+            fontSize: em(20, 24),
+            lineHeight: round(36 / 20),
+            marginTop: em(40, 20),
+            marginBottom: em(40, 20),
+            borderRadius: rem(8),
+            paddingTop: em(24, 20),
+            paddingRight: em(32, 20),
+            paddingBottom: em(24, 20),
+            paddingLeft: em(32, 20),
+          },
+          "& ol": {
+            marginTop: em(32, 24),
+            marginBottom: em(32, 24),
+            paddingLeft: em(38, 24),
+          },
+          "& ul": {
+            marginTop: em(32, 24),
+            marginBottom: em(32, 24),
+            paddingLeft: em(38, 24),
+          },
+          "& li": {
+            marginTop: em(12, 24),
+            marginBottom: em(12, 24),
+          },
+          "& ol > li": {
+            paddingLeft: em(10, 24),
+          },
+          "& ul > li": {
+            paddingLeft: em(10, 24),
+          },
+          "& > ul > li p": {
+            marginTop: em(20, 24),
+            marginBottom: em(20, 24),
+          },
+          "& > ul > li > *:first-child": {
+            marginTop: em(32, 24),
+          },
+          "& > ul > li > *:last-child": {
+            marginBottom: em(32, 24),
+          },
+          "& > ol > li > *:first-child": {
+            marginTop: em(32, 24),
+          },
+          "& > ol > li > *:last-child": {
+            marginBottom: em(32, 24),
+          },
+          "& ul ul, & ul ol, & ol ul, & ol ol": {
+            marginTop: em(16, 24),
+            marginBottom: em(16, 24),
+          },
+          "& hr": {
+            marginTop: em(72, 24),
+            marginBottom: em(72, 24),
+          },
+          "& hr + *": {
+            marginTop: "0",
+          },
+          "& h2 + *": {
+            marginTop: "0",
+          },
+          "& h3 + *": {
+            marginTop: "0",
+          },
+          "& h4 + *": {
+            marginTop: "0",
+          },
+          "& table": {
+            fontSize: em(20, 24),
+            lineHeight: round(28 / 20),
+          },
+          "& thead th": {
+            paddingRight: em(12, 20),
+            paddingBottom: em(16, 20),
+            paddingLeft: em(12, 20),
+          },
+          "& thead th:first-child": {
+            paddingLeft: "0",
+          },
+          "& thead th:last-child": {
+            paddingRight: "0",
+          },
+          "& tbody td, & tfoot td": {
+            paddingTop: em(16, 20),
+            paddingRight: em(12, 20),
+            paddingBottom: em(16, 20),
+            paddingLeft: em(12, 20),
+          },
+          "& tbody td:first-child, & tfoot td:first-child": {
+            paddingLeft: "0",
+          },
+          "& tbody td:last-child, & tfoot td:last-child": {
+            paddingRight: "0",
+          },
+          "& > :first-child": {
+            marginTop: "0",
+          },
+          "& > :last-child": {
+            marginBottom: "0",
+          },
+        },
       },
     },
-    defaultVariants: {
-      size: "base",
-    },
-    jsx,
   });
 }
 
