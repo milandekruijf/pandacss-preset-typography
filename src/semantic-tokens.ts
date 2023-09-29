dde14201aed991425ace950558db763e5f2aa7cb
Muijf <admin@muijf.com>
2023-09-29 08:29:55 +0200

    renamed to colorPalette

--- src/semantic-tokens.ts
+++ src/semantic-tokens.ts
@@ -5,65 +5,65 @@ import { DEFAULT_RECIPE_NAME } from "./constants";
 export function createDefaultSemanticTokens(
   options?: DefaultSemanticTokensOptions
 ): SemanticTokens {
-  const palette = options?.palette ?? "slate";
+  const colorPalette = options?.colorPalette ?? "slate";
 
   return defineSemanticTokens({
     colors: {
       [options?.prefix ?? DEFAULT_RECIPE_NAME]: {
         body: {
-          value: `{colors.${palette}.700}`,
+          value: `{colors.${colorPalette}.700}`,
         },
         heading: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         lead: {
-          value: `{colors.${palette}.600}`,
+          value: `{colors.${colorPalette}.600}`,
         },
         link: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         bold: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         counter: {
-          value: `{colors.${palette}.500}`,
+          value: `{colors.${colorPalette}.500}`,
         },
         bullet: {
-          value: `{colors.${palette}.300}`,
+          value: `{colors.${colorPalette}.300}`,
         },
         hrBorder: {
-          value: `{colors.${palette}.200}`,
+          value: `{colors.${colorPalette}.200}`,
         },
         quote: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         quoteBorder: {
-          value: `{colors.${palette}.200}`,
+          value: `{colors.${colorPalette}.200}`,
         },
         caption: {
-          value: `{colors.${palette}.500}`,
+          value: `{colors.${colorPalette}.500}`,
         },
         kbd: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         kbdShadow: {
           // Expects an RGB value
           value: "0 0 0",
         },
         code: {
-          value: `{colors.${palette}.900}`,
+          value: `{colors.${colorPalette}.900}`,
         },
         preCode: {
-          value: `{colors.${palette}.200}`,
+          value: `{colors.${colorPalette}.200}`,
         },
         preBg: {
-          value: `{colors.${palette}.800}`,
+          value: `{colors.${colorPalette}.800}`,
         },
         thBorder: {
-          value: `{colors.${palette}.300}`,
+          value: `{colors.${colorPalette}.300}`,
         },
         tdBorder: {
-          value: `{colors.${palette}.200}`,
+          value: `{colors.${colorPalette}.200}`,
         },
       },
     },
